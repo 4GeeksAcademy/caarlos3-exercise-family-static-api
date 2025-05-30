ed47d5eb6e7b53ee1efd16081970dbc341d2e899
CaarlosDev <crt.981995@gmail.com>
2025-05-30 18:03:47 +0000

    datas terminada

--- src/datastructures.py
+++ src/datastructures.py
@@ -43,17 +43,15 @@ class FamilyStructure:
     def add_member(self, member):
         
         if "id" is not member:
-            member = self._generate_id()
+            member['id'] = self._generate_id()
             self._members.append(member)
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        self._members = list(filter(lambda member: member['id'] != id, self._members))
 
     def get_member(self, id):
         for member in self._members:
-            if member == id:
+            if member['id'] == id:
               return member
 
 
